f88ca36e6f9df642faaff8def00c0d2a98708b47
Filipe Braga <lipebraga16@hotmail.com>
2021-03-15 13:37:31 -0300

    Primeiro commit de C#

---
+++ primeiros-passos-dot-net/conhecendo-csharp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Projeto1DIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroDeVezes = 5;
+            for (int i=0; i< numeroDeVezes; i++){
+                Console.WriteLine($"Bem vindo ao curso de .NET {i}");
+                
+            }
+            
+            //Console.WriteLine("Hello World!");
+            
+        }
+    }
+}
