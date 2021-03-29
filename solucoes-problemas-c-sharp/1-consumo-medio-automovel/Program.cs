2a6c0c0d6e598c488289e4a5dad604056f2d1534
Filipe Braga <lipebraga16@hotmail.com>
2021-03-29 11:45:25 -0300

    Resolvendo Desafio 1

---
+++ solucoes-problemas-c-sharp/1-consumo-medio-automovel/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsumoMedioAutomovel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dist;
+            double comb, media; //float - 4 bytes; double - 8 bytes; decimal - 16 bytes
+
+           // dist = Console.ReadLine(); //Não é possível converter implicitamente String (retorno do Console.Realine) para Int
+            dist = Convert.ToInt32(Console.ReadLine());
+            comb = Convert.ToDouble(Console.ReadLine());
+            media = dist / comb;
+
+            Console.WriteLine("{0:0.000} km/l", media);
+        }
+    }
+}
