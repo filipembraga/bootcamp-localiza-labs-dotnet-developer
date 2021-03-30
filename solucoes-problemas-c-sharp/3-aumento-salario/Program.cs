e64833c8bada2edc01349341cd6a2a91591c479d
Filipe Braga <lipebraga16@hotmail.com>
2021-03-30 10:12:39 -0300

    Corrigindo Desafio 3 conforme entrega final

--- solucoes-problemas-c-sharp/3-aumento-salario/Program.cs
+++ solucoes-problemas-c-sharp/3-aumento-salario/Program.cs
@@ -10,18 +10,26 @@ namespace _3_aumento_salario
             double salario, percentual, reajuste, novoSalario;
             salario = Convert.ToDouble(Console.ReadLine());
             
-            if(salario==0){
-                reajuste = salario * 0.15; 
-                novoSalario = salario + reajuste;
+            if(salario < 0.00){
+                reajuste = 0;
+                novoSalario = 0;
                 percentual = 0;
                 Console.WriteLine("Novo salario: {0:0.00}", novoSalario);
                 Console.WriteLine("Reajuste ganho: {0:0.00}", reajuste);
                 Console.WriteLine("Em percentual: {0} %", percentual);
             }
-            else if(salario >= 0.0 && salario <= 400.0){
+                else if(salario == 0.00){
+                reajuste = 0;
+                novoSalario = 0;
+                percentual = 15.00;
+                Console.WriteLine("Novo salario: {0:0.00}", novoSalario);
+                Console.WriteLine("Reajuste ganho: {0:0.00}", reajuste);
+                Console.WriteLine("Em percentual: {0} %", percentual);
+            }
+            else if(salario > 0.0 && salario <= 400.0){
                 reajuste = salario * 0.15;
                 novoSalario = salario + reajuste;
-                percentual = ((novoSalario - salario) * 100) / salario;
+                percentual = 15.00;
                 Console.WriteLine("Novo salario: {0:0.00}", novoSalario);
                 Console.WriteLine("Reajuste ganho: {0:0.00}", reajuste);
                 Console.WriteLine("Em percentual: {0} %", percentual);
@@ -29,7 +37,7 @@ namespace _3_aumento_salario
             else if(salario > 400.0 && salario <= 800.0){
                 reajuste = salario * 0.12;
                 novoSalario = salario + reajuste;
-                percentual = ((novoSalario - salario) * 100) / salario;
+                percentual = 12.00;
                 Console.WriteLine("Novo salario: {0:0.00}", novoSalario);
                 Console.WriteLine("Reajuste ganho: {0:0.00}", reajuste);
                 Console.WriteLine("Em percentual: {0} %", percentual);
@@ -37,7 +45,7 @@ namespace _3_aumento_salario
             else if(salario > 800.0 && salario <= 1200.0){
                 reajuste = salario * 0.10;
                 novoSalario = salario + reajuste;
-                percentual = Math.Round(((novoSalario - salario) * 100) / salario);
+                percentual = 10.00;
                 Console.WriteLine("Novo salario: {0:0.00}", novoSalario);
                 Console.WriteLine("Reajuste ganho: {0:0.00}", reajuste);
                 Console.WriteLine("Em percentual: {0} %", percentual);
@@ -45,7 +53,7 @@ namespace _3_aumento_salario
             else if(salario > 1200.0 && salario <= 2000.0){
                 reajuste = salario * 0.07;
                 novoSalario = salario + reajuste;
-                percentual = ((novoSalario - salario) * 100) / salario;
+                percentual = 7.00;
                 Console.WriteLine("Novo salario: {0:0.00}", novoSalario);
                 Console.WriteLine("Reajuste ganho: {0:0.00}", reajuste);
                 Console.WriteLine("Em percentual: {0} %", percentual);
@@ -53,7 +61,7 @@ namespace _3_aumento_salario
             else if(salario > 2000.0){
                 reajuste = salario * 0.04;
                 novoSalario = salario + reajuste;
-                percentual = Math.Round(((novoSalario - salario) * 100) / salario);
+                percentual = 4.00;
                 Console.WriteLine("Novo salario: {0:0.00}", novoSalario);
                 Console.WriteLine("Reajuste ganho: {0:0.00}", reajuste);
                 Console.WriteLine("Em percentual: {0} %", percentual);
