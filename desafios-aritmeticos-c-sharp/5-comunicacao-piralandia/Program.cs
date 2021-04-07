3986027d1437476fa4e5d631e9154e41d4dad92c
Filipe Braga <lipebraga16@hotmail.com>
2021-04-07 03:07:26 -0300

    Resolvendo Desafio 5

---
+++ desafios-aritmeticos-c-sharp/5-comunicacao-piralandia/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _5_comunicacao_piralandia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string n = Console.ReadLine();
+            string v = "";
+                
+            for (int i=0; i<n.Length; i++)
+            {
+                v += n[n.Length-i-1];
+            }
+
+            Console.WriteLine("{0}\n",v);
+        }
+    }
+}
