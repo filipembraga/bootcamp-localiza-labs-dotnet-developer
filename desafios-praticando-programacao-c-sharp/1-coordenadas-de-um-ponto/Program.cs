bb1fd931e3ce993715d3bf304ecc390e4ef74c17
Filipe Braga <lipebraga16@hotmail.com>
2021-04-26 18:13:10 -0300

    Resolvendo o Desafio 1

---
+++ desafios-praticando-programacao-c-sharp/1-coordenadas-de-um-ponto/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _1_coordenadas_de_um_ponto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, y;
+            string[] valores = Console.ReadLine().Split(' ');
+            x = double.Parse(valores[0]);
+            y = double.Parse(valores[1]);
+
+            if(x==0.0 & y==0.0){
+                Console.WriteLine("Origem");
+            } else if (x==0.0){
+                Console.WriteLine("Eixo Y");
+            } else if (y==0.0){
+                Console.WriteLine("Eixo X");
+            } else if (x>0.0 & y>0.0){
+                Console.WriteLine("Q1");
+            } else if (x<0.0 & y>0.0){
+                Console.WriteLine("Q2");
+            } else if (x<0.0 & y<0.0){
+                Console.WriteLine("Q3");
+            } else {
+                Console.WriteLine("Q4");
+            }
+        }
+    }
+}
