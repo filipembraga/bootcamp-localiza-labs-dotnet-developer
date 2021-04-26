95922e6b5b5833ea9bfe126afdbf26fd996e9b9c
Filipe Braga <lipebraga16@hotmail.com>
2021-04-26 18:54:52 -0300

    Resolvendo o Desafio 2

---
+++ desafios-praticando-programacao-c-sharp/2-compras-no-supermercado/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _2_compras_no_supermercado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var totalDeCasosDeTeste = int.Parse(Console.ReadLine());
+            SortedSet<string> listaCompras;
+            
+            for(int i=1; i<=totalDeCasosDeTeste; i++){
+                string[] valores = Console.ReadLine().Split(' ');
+                listaCompras = new SortedSet<string> (valores);
+                
+                foreach(var n in listaCompras){
+                    Console.Write(n.Replace(" ",""));
+                    Console.Write(' ');
+                }
+                listaCompras.Clear();
+                Console.WriteLine();
+            }
+        }
+    }
+}
